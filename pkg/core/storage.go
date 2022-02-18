082d23db337207de357e6f364ea4e8a78b68728e
Grumbleur <github@grumbleur.fr>
2022-02-18 18:04:17 +0100

    clean: Fix documentation tipo

--- pkg/core/storage.go
+++ pkg/core/storage.go
@@ -4,6 +4,6 @@ type IStorage interface {
 	// Get the name of the storage
 	// Will be unique across all storages
 	Name() string
-	//  Method call when insert new data in the storage
+	// Method call when insert new data in the storage
 	Push(value interface{}) error
 }
