c25ef4e8ccc755ec566bd1be36e80ed538b3176c
Gregory DALMAR <contact@atomys.fr>
2022-02-19 14:21:10 +0100

    feat: Load and store security layer from contiguration

--- internal/config/structs.go
+++ internal/config/structs.go
@@ -1,16 +1,18 @@
 package config
 
+import "42stellar.org/webhooks/pkg/factory"
+
 type Configuration struct {
 	APIVersion string         `mapstructure:"apiVersion"`
 	Specs      []*WebhookSpec `mapstructure:"specs"`
 }
 
 type WebhookSpec struct {
-	Name          string                  `mapstructure:"name"`
-	EntrypointURL string                  `mapstructure:"entrypointUrl"`
-	Security      map[string]SecuritySpec `mapstructure:"security"`
-	Storage       map[string]StorageSpec  `mapstructure:"storage"`
+	Name              string                              `mapstructure:"name"`
+	EntrypointURL     string                              `mapstructure:"entrypointUrl"`
+	Security          []map[string]map[string]interface{} `mapstructure:"security"`
+	SecurityFactories []*factory.Factory                  `mapstructure:"-"`
+	Storage           map[string]StorageSpec              `mapstructure:"storage"`
 }
 
-type SecuritySpec struct{}
 type StorageSpec struct{}
