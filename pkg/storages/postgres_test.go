6a7195ceecfae3ac41f3a2754de997c7bc3ad470
Grumbleur <remi.gaiffe@googlemail.com>
2022-02-22 23:04:48 +0100

    test: Edit assert nil per assert error or no error and add test more function.

--- pkg/storages/postgres_test.go
+++ pkg/storages/postgres_test.go
@@ -17,17 +17,17 @@ type PostgresSetupTestSuite struct {
 func (suite *PostgresSetupTestSuite) BeforeTest(suiteName, testName string) {
 	var err error
 	if suite.client, err = sql.Open("postgres", "postgresql://webhook:test@127.0.0.1:5432/webhook_db?sslmode=disable"); err != nil {
-		assert.Error(suite.T(), err)
+		suite.T().Error(err)
 	}
 	if _, err := suite.client.Query("CREATE TABLE test (test_field TEXT)"); err != nil {
-		assert.Error(suite.T(), err)
+		suite.T().Error(err)
 	}
 }
 
 // Delete Table after test
 func (suite *PostgresSetupTestSuite) AfterTest(suiteName, testName string) {
 	if _, err := suite.client.Query("DROP TABLE test"); err != nil {
-		assert.Error(suite.T(), err)
+		suite.T().Error(err)
 	}
 }
 
@@ -38,23 +38,36 @@ func TestPostgresName(t *testing.T) {
 
 func TestPostgresNewPostgresStorage(t *testing.T) {
 	_, err := NewPostgresStorage(map[string]interface{}{
+		"databaseURL": []int{1},
+	})
+	assert.Error(t, err)
+
+	_, err = NewPostgresStorage(map[string]interface{}{
 		"databaseURL": "postgresql://webhook:test@127.0.0.1:5432/webhook_db?sslmode=disable",
 		"tableName":   "test",
 		"dataField":   "test_field",
 	})
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 }
 
 func (suite *PostgresSetupTestSuite) TestPostgresPush() {
 	newClient, err := NewPostgresStorage(map[string]interface{}{
 		"databaseURL": "postgresql://webhook:test@127.0.0.1:5432/webhook_db?sslmode=disable",
+		"tableName":   "Not Exist",
+		"dataField":   "Not exist",
+	})
+	err = newClient.Push("Hello")
+	assert.Error(suite.T(), err)
+
+	newClient, err = NewPostgresStorage(map[string]interface{}{
+		"databaseURL": "postgresql://webhook:test@127.0.0.1:5432/webhook_db?sslmode=disable",
 		"tableName":   "test",
 		"dataField":   "test_field",
 	})
-	assert.Nil(suite.T(), err)
+	assert.NoError(suite.T(), err)
 
 	err = newClient.Push("Hello")
-	assert.Nil(suite.T(), err)
+	assert.NoError(suite.T(), err)
 }
 
 func TestRunPostgresPush(t *testing.T) {
