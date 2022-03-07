8e150e056077a410a89aba5788375a87d13a8a17
Gregory DALMAR <contact@atomys.fr>
2022-03-07 04:09:49 +0100

    chore(security/pipeline): Cleanup header tests

--- pkg/factory/f_header_test.go
+++ pkg/factory/f_header_test.go
@@ -10,48 +10,6 @@ import (
 	"42stellar.org/webhooks/internal/valuable"
 )
 
-// func Test_f_header(t *testing.T) {
-// 	assert := assert.New(t)
-
-// 	headerName := "X-Token"
-// 	header := make(http.Header)
-// 	header.Add(headerName, "test")
-
-// 	request := httptest.NewRequest("POST", "/", nil)
-
-// 	tests := []struct {
-// 		name    string
-// 		header  http.Header
-// 		want    string
-// 		wantErr bool
-// 	}{
-// 		{"no config will fail", nil, "", true},
-// 		{"no values", nil, "", false},
-// 		{"no inputs", nil, "", true},
-// 		{"valid config but no header set", nil, "", false},
-// 		{"valid config and header set", header, "test", false},
-// 		{"invalid config name and no header set", nil, "", false},
-// 		{"invalid config name but header set", header, "", false},
-// 		{"invalid config map", nil, "", true},
-// 		{"invalid input type", nil, "", true},
-// 	}
-// 	for _, test := range tests {
-// 		factory := newFactory(&headerFactory{})
-// 		request.Header = header
-// 		factory.WithInput("request", request)
-// 		factory.WithInput("headerName", &InputConfig{Valuable: valuable.Valuable{Value: &headerName}})
-
-// 		err := factory.Run()
-// 		if test.wantErr {
-// 			assert.Error(err, test.name)
-// 		} else {
-// 			assert.NoError(err, test.name)
-// 		}
-
-// 		assert.Equal(test.want, factory.Outputs[0].Value, test.name)
-// 	}
-// }
-
 type testSuiteFactoryHeader struct {
 	suite.Suite
 	request  *http.Request
