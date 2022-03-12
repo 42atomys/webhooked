ab52e255a2dde59a5d623ddd7cfef711a0405f1c
Gregory DALMAR <contact@atomys.fr>
2022-03-12 21:09:57 +0100

    fix(security/pipeline): Factory failed when multiple specs is used in same configuration

--- pkg/factory/registry.go
+++ pkg/factory/registry.go
@@ -7,20 +7,20 @@ import (
 var (
 	// FunctionMap contains the map of function names to their respective functions
 	// This is used to validate the function name and to get the function by name
-	factoryMap = map[string]*Factory{
-		"debug":           newFactory(&debugFactory{}),
-		"header":          newFactory(&headerFactory{}),
-		"compare":         newFactory(&compareFactory{}),
-		"hasPrefix":       newFactory(&hasPrefixFactory{}),
-		"hasSuffix":       newFactory(&hasSuffixFactory{}),
-		"generateHmac256": newFactory(&generateHMAC256Factory{}),
+	factoryMap = map[string]IFactory{
+		"debug":           &debugFactory{},
+		"header":          &headerFactory{},
+		"compare":         &compareFactory{},
+		"hasPrefix":       &hasPrefixFactory{},
+		"hasSuffix":       &hasSuffixFactory{},
+		"generateHmac256": &generateHMAC256Factory{},
 	}
 )
 
 // GetFunctionByName returns true if the function name is contained in the map
 func GetFactoryByName(name string) (*Factory, bool) {
 	fn, ok := factoryMap[name]
-	return fn, ok
+	return newFactory(fn), ok
 }
 
 // Register a new factory in the factory map with the built-in factory name
@@ -28,6 +28,6 @@ func Register(factory IFactory) error {
 	if _, ok := GetFactoryByName(factory.Name()); ok {
 		return fmt.Errorf("factory %s is already exist", factory.Name())
 	}
-	factoryMap[factory.Name()] = newFactory(factory)
+	factoryMap[factory.Name()] = factory
 	return nil
 }
