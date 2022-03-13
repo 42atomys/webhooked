e01f2c8591083ae4d93f294cabaf8ec1da267934
42Atomys <contact@atomys.fr>
2022-03-13 17:56:08 +0100

    feat(logging): Add prometheus metrics & structure logs (#45)
    * feat(logging): Add prometheus histogram
    
    * feat(observability): Allow user to toggle /metrics endpoint and prometheus strategy (disable by default)
    
    * chore(github/actions): Use tests config file instead of example
    
    * feat(observability): Structure middlewares and tests
    
    * chore(configuration): Move log in right place
    
    * chore(logging): Add more debug logs
    
    * chore(configuration): Rework error messages on storage
    
    * chore(configuration): Rework error messages on configuration validation
    
    * chore(configuration): Rework error messages on security load
    
    * chore(server): Rework debug logs on handlers
    
    * docs(server): Write documentation for server v1alpha1
    
    * docs(storage): Rewrite documentation of storages
    
    * docs(storage): Fix misspell on documentation
    
    * docs(configuration): Add structs documentation for configuration
    
    * docs(readme): Update roadmap file

--- pkg/storage/rabbitmq/rabbitmq.go
+++ pkg/storage/rabbitmq/rabbitmq.go
@@ -35,7 +35,7 @@ type config struct {
 // Run is made from external caller at begins programs
 // @param config contains config define in the webhooks yaml file
 // @return RabbitMQStorage the struct contains client connected and config
-// @error an error if the the client is not initialized succesfully
+// @return an error if the the client is not initialized successfully
 func NewStorage(configRaw map[string]interface{}) (*storage, error) {
 	var err error
 
