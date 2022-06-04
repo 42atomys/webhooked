38b87859d43b436c84f9306608509d4b22a9c84b
Atomys <contact@atomys.fr>
2022-06-04 20:09:31 +0200

    fix: Prevent segfault when no global is defined

--- internal/config/specification.go
+++ internal/config/specification.go
@@ -7,7 +7,7 @@ func (s WebhookSpec) HasSecurity() bool {
 
 // HasGlobalFormating returns true if the spec has a global formating
 func (s WebhookSpec) HasGlobalFormating() bool {
-	return s.Formating.TemplatePath != "" || s.Formating.TemplateString != ""
+	return s.Formating != nil && (s.Formating.TemplatePath != "" || s.Formating.TemplateString != "")
 }
 
 // HasFormating returns true if the storage spec has a formating
