430288156c7ab6664e367a64b14770e158ed8c1c
42Atomys <contact@atomys.fr>
2022-09-10 14:38:54 +0200

    fix: rabbitmq will reconnect if the channel is closed (#99)
    * fix: rabbitmq will reconnect if the channel is closed
    
    * chore: ensure push fail after 5 attempt

--- pkg/storage/rabbitmq/rabbitmq_test.go
+++ pkg/storage/rabbitmq/rabbitmq_test.go
@@ -68,7 +68,33 @@ func TestRunRabbitMQPush(t *testing.T) {
 }
 
 func TestContentType(t *testing.T) {
-	assert.Equal(t, "text/plain", config{}.ContentType())
-	assert.Equal(t, "text/plain", config{DefinedContentType: ""}.ContentType())
-	assert.Equal(t, "application/json", config{DefinedContentType: "application/json"}.ContentType())
+	assert.Equal(t, "text/plain", (&config{}).ContentType())
+	assert.Equal(t, "text/plain", (&config{DefinedContentType: ""}).ContentType())
+	assert.Equal(t, "application/json", (&config{DefinedContentType: "application/json"}).ContentType())
+}
+
+func TestReconnect(t *testing.T) {
+	if testing.Short() {
+		t.Skip("rabbitmq testing is skiped in short version of test")
+		return
+	}
+
+	newClient, err := NewStorage(map[string]interface{}{
+		"databaseUrl":      "amqp://user:password@127.0.0.1:5672",
+		"queueName":        "hello",
+		"contentType":      "text/plain",
+		"durable":          false,
+		"deleteWhenUnused": false,
+		"exclusive":        false,
+		"noWait":           false,
+		"mandatory":        false,
+		"immediate":        false,
+	})
+	assert.NoError(t, err)
+
+	assert.NoError(t, newClient.Push("Hello"))
+	assert.NoError(t, newClient.client.Close())
+	assert.NoError(t, newClient.Push("Hello"))
+	assert.NoError(t, newClient.channel.Close())
+	assert.NoError(t, newClient.Push("Hello"))
 }
